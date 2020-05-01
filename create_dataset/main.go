ca93df90b707badf2f0363cf53fec0d3fadb977d
Alex Nichol <unixpickle@gmail.com>
2020-05-01 17:08:37 -0400

    use power heuristic

--- create_dataset/main.go
+++ create_dataset/main.go
@@ -94,6 +94,7 @@ func SaveScene(outDir string, obj render3d.Object, rend *render3d.RecursiveRayTr
 	bidir.MaxStddev = 0.005 / scale
 	bidir.OversaturatedStddevs = 3
 	bidir.RouletteDelta = 0.05 / scale
+	bidir.PowerHeuristic = 2
 
 	var lastFrac float64
 	bidir.LogFunc = func(frac, samples float64) {
