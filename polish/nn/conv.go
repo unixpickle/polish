0703daae60e92fefd885d31803bc3a8f86f6ded6
Alex Nichol <unixpickle@gmail.com>
2020-05-05 13:45:31 -0400

    convolution implementation

--- polish/nn/conv.go
+++ polish/nn/conv.go
@@ -1,5 +1,69 @@
 package nn
 
+// Conv is a 2D convolution operator.
+//
+// It contains weights of the shape:
+//
+//     [out_depth x in_depth x kernel_size x kernel_size]
+//
+type Conv struct {
+	OutDepth   int
+	InDepth    int
+	KernelSize int
+	Stride     int
+	Weights    []float32
+}
+
+// Apply applies the convolution to a Tensor.
+//
+// The resulting Tensor's size is determined by
+// ConvOutputSize().
+func (c *Conv) Apply(t *Tensor) *Tensor {
+	if t.Depth != c.InDepth {
+		panic("input Tensor does not have the correct number of channels")
+	}
+	outH, outW := ConvOutputSize(t.Height, t.Width, c.KernelSize, c.Stride)
+	out := NewTensor(outH, outW, c.OutDepth)
+
+	var outIdx int
+	features := c.transposedFeatures()
+	Patches(t, c.KernelSize, c.Stride, func(patch *Tensor) {
+		for _, feature := range features {
+			var dot float32
+			for i, x := range patch.Data {
+				dot += x * feature.Data[i]
+			}
+			out.Data[outIdx] = dot
+			outIdx++
+		}
+	})
+
+	return out
+}
+
+func (c *Conv) transposedFeatures() []*Tensor {
+	featureStride := c.KernelSize * c.KernelSize * c.InDepth
+	var featureIdx int
+	var result []*Tensor
+	for i := 0; i < c.OutDepth; i++ {
+		feature := c.Weights[featureIdx : featureIdx+featureStride]
+		featureIdx += featureStride
+
+		tensor := NewTensor(c.KernelSize, c.KernelSize, c.InDepth)
+		var idx int
+		for y := 0; y < tensor.Height; y++ {
+			for x := 0; x < tensor.Width; x++ {
+				for z := 0; z < tensor.Depth; z++ {
+					tensor.Data[idx] = feature[(y+z*c.KernelSize)*c.KernelSize+x]
+					idx++
+				}
+			}
+		}
+		result = append(result, tensor)
+	}
+	return result
+}
+
 // Patches extracts image patches for a convolution of the
 // given kernel size and stride, and calls f with each
 // patch.
