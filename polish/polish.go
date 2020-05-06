9b68352caf3b200e4d701395c35563a3efaba6b2
Alex Nichol <unixpickle@gmail.com>
2020-05-06 12:55:30 -0400

    fix output padding

--- polish/polish.go
+++ polish/polish.go
@@ -12,22 +12,16 @@ import (
 // feature channels besides RGB colors.
 func PolishImage(t ModelType, img image.Image) image.Image {
 	inTensor := nn.NewTensorRGB(img)
-	inTensor = padInput(t, inTensor)
-	outTensor := t.Layer().Apply(inTensor)
-	outTensor = unpadInput(t, outTensor)
+	pad, unpad := padAndUnpad(t, inTensor)
+	outTensor := pad.Apply(inTensor)
+	outTensor = t.Layer().Apply(outTensor)
+	outTensor = unpad.Apply(outTensor)
 	return outTensor.RGB()
 }
 
-func padInput(t ModelType, in *nn.Tensor) *nn.Tensor {
+func padAndUnpad(t ModelType, in *nn.Tensor) (pad, unpad nn.Layer) {
 	lcd := t.LCD()
-	rightPad := in.Width % lcd
-	bottomPad := in.Height % lcd
-	return in.Pad(0, rightPad, bottomPad, 0)
-}
-
-func unpadInput(t ModelType, in *nn.Tensor) *nn.Tensor {
-	lcd := t.LCD()
-	rightPad := in.Width % lcd
-	bottomPad := in.Height % lcd
-	return in.Unpad(0, rightPad, bottomPad, 0)
+	rightPad := (lcd - in.Width%lcd) % lcd
+	bottomPad := (lcd - in.Height%lcd) % lcd
+	return nn.NewPad(0, rightPad, bottomPad, 0), nn.NewUnpad(0, rightPad, bottomPad, 0)
 }
