d7e4a5479efd4cb7d5bad2f62022647e2e0e7f06
Alex Nichol <unixpickle@gmail.com>
2020-05-07 18:57:20 -0400

    lower memory usage for deconvolution
    And it's about 2x as fast on the benchmark, quite shockingly.

--- polish/nn/deconv.go
+++ polish/nn/deconv.go
@@ -30,16 +30,34 @@ func (d *Deconv) Apply(t *Tensor) *Tensor {
 	outH, outW := DeconvOutputSize(t.Height, t.Width, d.KernelSize, d.Stride)
 	features := d.transposedFeatures()
 
-	return addDeconvPatches(t, func() *Tensor {
-		return NewTensor(outH, outW, d.OutDepth)
-	}, func(out *Tensor, x, y int, data []float32) {
-		outX := x * d.Stride
-		outY := y * d.Stride
+	out := NewTensor(outH, outW, d.OutDepth)
+	lock := sync.Mutex{}
+
+	d.iteratePatches(t, func(tmp *Tensor, x, y int, data []float32) {
 		for i, scale := range data {
 			feature := features[i]
-			addPatch(out, feature, outX, outY, scale)
+			if i == 0 {
+				for j, x := range feature.Data {
+					tmp.Data[j] = x * scale
+				}
+			} else {
+				for j, x := range feature.Data {
+					tmp.Data[j] += x * scale
+				}
+			}
 		}
+		outX := x * d.Stride
+		outY := y * d.Stride
+
+		// Adding to the global image must be synchronized,
+		// but it takes up so little of the computation time
+		// that it doesn't seem to matter.
+		lock.Lock()
+		addPatch(out, tmp, outX, outY)
+		lock.Unlock()
 	})
+
+	return out
 }
 
 func (d *Deconv) transposedFeatures() []*Tensor {
@@ -65,25 +83,18 @@ func (d *Deconv) transposedFeatures() []*Tensor {
 	return result
 }
 
-func addDeconvPatches(t *Tensor, makeOut func() *Tensor,
-	f func(out *Tensor, x, y int, data []float32)) *Tensor {
+func (d *Deconv) iteratePatches(t *Tensor, f func(tmp *Tensor, x, y int, data []float32)) {
 	numGos := runtime.GOMAXPROCS(0)
-
-	tensors := make([]*Tensor, numGos)
-	for i := range tensors {
-		tensors[i] = makeOut()
-	}
-
 	var wg sync.WaitGroup
 	for i := 0; i < numGos; i++ {
 		wg.Add(1)
 		go func(goIdx int) {
 			defer wg.Done()
-			out := tensors[goIdx]
+			tmp := NewTensor(d.KernelSize, d.KernelSize, d.OutDepth)
 			idx := goIdx * t.Width * t.Depth
 			for y := goIdx; y < t.Height; y += numGos {
 				for x := 0; x < t.Width; x++ {
-					f(out, x, y, t.Data[idx:idx+t.Depth])
+					f(tmp, x, y, t.Data[idx:idx+t.Depth])
 					idx += t.Depth
 				}
 				idx += (numGos - 1) * t.Width
@@ -91,24 +102,16 @@ func addDeconvPatches(t *Tensor, makeOut func() *Tensor,
 		}(i)
 	}
 	wg.Wait()
-
-	sum := tensors[0]
-	for _, t1 := range tensors[1:] {
-		for i, x := range t1.Data {
-			sum.Data[i] += x
-		}
-	}
-	return sum
 }
 
-func addPatch(dst, src *Tensor, outX, outY int, scale float32) {
+func addPatch(dst, src *Tensor, outX, outY int) {
 	var srcIdx int
 	dstIdx := (outX + outY*dst.Width) * dst.Depth
 	dstStride := dst.Width*dst.Depth - src.Width*src.Depth
 	for y := 0; y < src.Height; y++ {
 		for x := 0; x < src.Width; x++ {
 			for z := 0; z < src.Depth; z++ {
-				dst.Data[dstIdx] += src.Data[srcIdx] * scale
+				dst.Data[dstIdx] += src.Data[srcIdx]
 				dstIdx++
 				srcIdx++
 			}
